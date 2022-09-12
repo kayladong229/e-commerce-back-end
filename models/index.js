51845bf8eed62e50bed027e1cdcaf3cbb3df642b
kayladong229 <105991864+kayladong229@users.noreply.github.com>
2022-09-12 16:06:12 -0700

    Added draft code to models files

--- models/index.js
+++ models/index.js
@@ -5,12 +5,15 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+ProductTag.belongsTo(category, )
 
 // Categories have many Products
+Category.hasMany(Product, )
 
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongToMany(ProductTag, )
 // Tags belongToMany Products (through ProductTag)
+ProductTag.belongToMany(Product, )
 
 module.exports = {
   Product,
